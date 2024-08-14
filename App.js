914e32b651a9392989189efa082848112579b14e
Ravi Kumar <mailmeonravikumar@gmail.com>
2024-08-14 06:52:47 +0530

    Episode 2: Igniting our app.

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
-// creating a nested html structure using createElement.
-
-{
-  /* 
-  <div id="parent">
-    <div id="child">
-      <h1>I am a h1 tag</h1>
-    </div>
-    <div id="child2">
-      <h2>I am a h2 tag</h2>
-    </div>
-  </div> 
-*/
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement(
     "div",
     { id: "child" },
-    React.createElement("h1", {}, "I am a h1 tag")
+    React.createElement("h1", {}, "Namaste React course🚀")
   ),
   React.createElement(
     "div",
@@ -26,11 +14,6 @@ const parent = React.createElement("div", { id: "parent" }, [
   ),
 ]);
 
-console.log(parent); // object
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
-
-// React.createElement ==> creates an object.
-// the render method takes the object and render it on the DOM.
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Namaste React ✈️👨🏻‍💻
+# Namaste React ✈️👨🏻‍💻
+
+# Parcel
+
+- Dev Build.
+- Local server.
+- HMR = Hot Module Replacement.
+- File watching algorithm - written in C++.
+- Caching - Faster builds. => .parcel-cacha
